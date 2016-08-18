a7ba99d7be5ace01f1f8ab2060c524581be91815
Yuen-iMac <rxg9527@sina.cn>
2016-08-18 17:10:34 +0800

    initial commit

---
+++ Objective-C_Runtime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C_Runtime
+//
+//  Created by Vincent on 16/8/18.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
