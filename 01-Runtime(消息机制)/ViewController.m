55d187fb485140d3c01680731e4f926eb08f2138
Yuen-iMac <rxg9527@sina.cn>
2016-08-18 17:53:07 +0800

    0818 方法调用流程

--- 01-Runtime(消息机制)/ViewController.m
+++ 01-Runtime(消息机制)/ViewController.m
@@ -29,6 +29,13 @@
     p = objc_msgSend(p, sel_registerName("init"));
     objc_msgSend(p, @selector(eat));
     objc_msgSend(p, @selector(run:),20);
+    
+    // 方法调用流程
+    // 怎么去调用eat方法 , 对象方法:类对象的方法列表 类方法:元类中方法列表
+    // 1.通过isa去对应的类中查找
+    // 2.注册方法编号
+    // 3.根据方法编号去查找对应方法
+    // 4.找到只是最终函数实现地址,根据地址去方法区调用对应函数
 }
 
 @end
